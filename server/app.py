bcf22713c53596eb40336142e9007fe201eb2b87
mararo-ops <danielmararo1@gmail.com>
2024-01-19 12:05:07 +0300

    complete assignment

--- server/app.py
+++ server/app.py
@@ -20,15 +20,33 @@ def clear_session():
     session['page_views'] = 0
     return {'message': '200: Successfully cleared session data.'}, 200
 
-@app.route('/articles')
+@app.route('/articles',methods=['GET'])
 def index_articles():
 
     pass
 
-@app.route('/articles/<int:id>')
+@app.route('/articles/<int:id>',methods=['GET'])
 def show_article(id):
+    if 'page_views' not in session:
+      session['page_views'] =0
 
-    pass
+    # Increment the 'page_views' by 1 for each request.
+    session['page_views'] += 1
 
+    # Check if the user has viewed more than 3 pages.
+    if session['page_views'] > 3:
+        # Render a JSON response with an error message and status code 401.
+        return jsonify({'message': 'Maximum pageview limit reached'}), 401
+     
+    article_data = {
+        'author': 'john',
+        'title': 'Sample Article',
+        'content': 'This is a sample article content.',
+        'preview': 'This is a preview of the article.',
+        'minutes_to_read':'20',
+        'date':'12/03/2020'
+    }
+
+    return make_response(jsonify(article_data), 200)
 if __name__ == '__main__':
     app.run(port=5555)
